0601710c017a993a70e6fbb94897e162ae80a99d
okj15 <32460725+okj15@users.noreply.github.com>
2023-03-19 23:47:07 +0900

    use local storage

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var app = new Vue({
   el: '#app',
   data: {
-    items: [],
-    newItem: ''
+    newItem: '',
+    items: []
+  },
+  mounted() {
+    // localStorage からデータを読み込む
+    if (localStorage.getItem('items')) {
+      try {
+        this.items = JSON.parse(localStorage.getItem('items'));
+      } catch (e) {
+        localStorage.removeItem('items');
+      }
+    }
   },
   methods: {
     addItem: function () {
-      if (this.newItem.trim() !== '') {
-        this.items.push(this.newItem);
-        this.newItem = '';
+      if (this.newItem.trim() === '') {
+        return;
       }
+      this.items.push(this.newItem.trim());
+      this.newItem = '';
+      this.saveData();
     },
     removeItem: function (index) {
       this.items.splice(index, 1);
+      this.saveData();
+    },
+    saveData: function () {
+      localStorage.setItem('items', JSON.stringify(this.items));
     }
   }
 });
